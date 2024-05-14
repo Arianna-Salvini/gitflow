97963ee22046fff48fc6310bd4bf70b4221a697c
MarioBroi <inoix16@gmail.com>
2024-05-14 11:12:23 +0100

    add contact.blade in guest.folder, add route in web.php

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('guests.about');
 });
+
+Route::get('/contact', function () {
+    return view('guests.contact');
+});
